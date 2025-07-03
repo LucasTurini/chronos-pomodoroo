7a22eccc35bddb3c58b38a718dec214e9d6073c2
Lucas Turini <lucasturini0406@gmail.com>
2025-07-02 21:11:49 -0300

    criando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
+
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 
 
@@ -10,11 +13,15 @@ export function App() {
   return (
     <>
         <Container>
-          <Heading>LOGO</Heading>
+            <Logo/>         
         </Container>
 
         <Container>
-          <Heading>MENU</Heading>
+          <Menu/>
+        </Container>
+
+        <Container>
+          <CountDown/>
         </Container>
 
     
