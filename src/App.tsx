1e5d05a730c5306eef164f51fa4f4f50b41ba79a
Lucas Turini <lucasturini0406@gmail.com>
2025-07-22 14:01:17 -0300

    button and footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 
 
@@ -41,9 +44,13 @@ export function App() {
               </div>
 
               <div className='formRow'>
-                  <button>Envair</button>
+                <DefaultButton icon={<PlayCircleIcon/>} />
               </div>
           </form>
+        </Container>
+
+        <Container>
+            <Footer/>
         </Container>
 
     
